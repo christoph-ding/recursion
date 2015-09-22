90f9f0d0d3a77ab567726464b3d59c1006ae9890
Pokegear <christoph.ding@gmail.com>
2015-09-21 23:23:05 -0500

    "Made both functions work.  The only change I made to the GetElementsByClass wasa 'console.log' line for debugging purpose.  Learned so much!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  // default values 
+  function recur(className, root) {
+    var elements_which_fit = [];
+    var root = root || document.body;
+    var root_classes = root.classList;
+    
+    // Base Case 
+    // Does root CONTAIN className?  OH WAIT, we cannot use CONTAIN, so we have to iterate...        
+    for (var i = 0; i < root_classes.length; i++) {
+      if (root_classes[i] == className) {
+        var current_node = root;
+        elements_which_fit.push(current_node);
+      };
+    }
+      
+    // Recursive Case
+    // recurse on children
+    if (root.childNodes.length > 0 ) {
+      var current_children = root.children;    
+      for (var i = 0; i < current_children.length; i++) {
+      // that child is its own 'tree'
+      var child_elements_filtered = recur(className, current_children[i]);
+      elements_which_fit = elements_which_fit.concat(child_elements_filtered);
+      };
+    };
+
+    return elements_which_fit;
+    };
+
+  return recur(classNames);
 };
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+function stringifyJSON(obj) {
+
+  // debugging logs
+  
+  // have logic based on type of data obj
+
+  // NON COLLECTION  
+  if (typeof(obj) === "undefined")   
+      {return;}
+
+  else if (typeof(obj) === "number" || typeof(obj) === "boolean" || obj === null) 
+      {return "" + obj + ""}
+
+  else if (typeof(obj) === "string") {
+  return "\"" + obj + "\"";
+  };  
+
+  // COLLECTIONS
+
+  var stringified_elements = [];
+  
+  // Arrays
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++ ) {
+      stringified_elements.push(stringifyJSON(obj[i]));
+    };
+    return "[" + stringified_elements.join(",") + "]";
+    };
+
+  // Objects
+  if (typeof(obj) === "object") {
+    for (var key in obj) {    
+      // we should not stringify functions or undefined
+      if (!(typeof(obj[key]) === "undefined" || typeof(obj[key]) === "function")) { 
+        var stringified_key_value_pair;  
+        stringified_key_value_pair = stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+        stringified_elements.push(stringified_key_value_pair) 
+        };
+    }
+    return "{" + stringified_elements.join(',') + "}"; 
+  };
 };
